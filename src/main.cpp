4d537a1123d4ddd25fd3506e87f75ef6603044da
random-zebra <random.zebra@protonmail.com>
2020-05-21 16:14:25 +0200

    [qt] Properly display required fee instead of minTxFee
    backports bitcoin@abd8b768ee889f28b3d2bc209307a9867a973556 and
    bitcoin/53238ff0b1085352e4aaa796d0e473551e573143
    # Conflicts:
    #	src/init.cpp
    #	src/main.cpp
    #	src/qt/coincontroldialog.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -113,7 +113,7 @@ unsigned int nCoinCacheSize = 5000;
 /* If the tip is older than this (in seconds), the node is considered to be in initial block download. */
 int64_t nMaxTipAge = DEFAULT_MAX_TIP_AGE;
 
-/** Fees smaller than this (in uvit) are considered zero fee (for relaying and mining)
+/** Fees smaller than this (in uvit) are considered zero fee (for relaying, mining and transaction creation)
  * We are ~100 times smaller then bitcoin now (2015-06-23), set minRelayTxFee only 10 times higher
  * so it's still 10 times lower comparing to bitcoin.
  */
