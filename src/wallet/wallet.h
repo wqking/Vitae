3cf57afe42f944c376183230771bc10b556775ae
furszy <matiasfurszyfer@protonmail.com>
2020-06-02 20:50:53 -0300

    Merge #1659: Few compiler warnings fixed.
    62cbdbd013b8786ba4fbc543c825a9d1da419420 clang compiler warnings fixed. (furszy)
    
    Pull request description:
    
    ACKs for top commit:
      random-zebra:
        utACK 62cbdbd013b8786ba4fbc543c825a9d1da419420
      Fuzzbawls:
        ACK 62cbdbd013b8786ba4fbc543c825a9d1da419420
    
    Tree-SHA512: 6ccb2340f62c6eb49901a0e69798deafc9c4f81883796357798b0333b2db19fe7bce7e5468d0fef1b6a4001bcd71362315750d0a016316b91eb2d41d9e2e6a6b
    
    # Conflicts:
    #	src/main.cpp

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -677,7 +677,7 @@ public:
 typedef std::map<std::string, std::string> mapValue_t;
 
 
-static void ReadOrderPos(int64_t& nOrderPos, mapValue_t& mapValue)
+static inline void ReadOrderPos(int64_t& nOrderPos, mapValue_t& mapValue)
 {
     if (!mapValue.count("n")) {
         nOrderPos = -1; // TODO: calculate elsewhere
@@ -687,7 +687,7 @@ static void ReadOrderPos(int64_t& nOrderPos, mapValue_t& mapValue)
 }
 
 
-static void WriteOrderPos(const int64_t& nOrderPos, mapValue_t& mapValue)
+static inline void WriteOrderPos(const int64_t& nOrderPos, mapValue_t& mapValue)
 {
     if (nOrderPos == -1)
         return;
